906abd0f915653dd9493a11c0eb356a805447a2b
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-03-24 03:46:15 +0900

    手動でニュースを取得する方法を追加

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,22 @@
+import { Message } from "discord.js";
+import { fetchNews, parseNews } from "../utils/fetchNews";
+import { sortTypes } from "../@types/sortType";
+
+const MessageCreate = async (message: Message) => {
+    if (message.author.bot) return;
+    switch (message.content) {
+        case "nb!ping":
+            message.reply("Pong!");
+            break;
+        case "nb!manual":
+            message.reply({
+                embeds: [
+                    parseNews((
+                        await fetchNews(["automaton-media.com"], sortTypes.PUBLISHED_AT)
+                    ))
+                ]
+            }
+            );
+            break;
+    }
+};
